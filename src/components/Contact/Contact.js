d2e2ee0a90df98d3a29f4fbdb0d75224f10800fe
Anudeep <anudeepreddyk4@gmail.com>
2020-12-09 22:17:09 +0530

    added contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import './contact.css'
 
 const Contact = () => {
     return (
-        <div>
-            Contact
+        <div className="contact">
+            <div className="online-contact">
+            <h1>Sales and Product Inquiries</h1>
+            <h2>Apple Online Store</h2>
+            <p>
+            <a href="https://www.apple.com/">Apple.com</a> is a convenient place to purchase Apple products and accessories from Apple. You can buy online or call 000800 040 1966.
+            </p>
+            </div>
+
+            <div className="online-contact">
+            <h1>How to Buy for Education</h1>
+            <p>
+            If you are a student or teacher, visit the Apple Store for Education or call 000800 040 1966.
+            </p>
+            <h2>Find Apple Authorised Resellers</h2>
+            </div>
         </div>
     )
 }
